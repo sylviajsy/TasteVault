a34e59f77192b362f104060ac02d30e55991c337
Siyi Ji <sylviajsyy@gmail.com>
2026-05-04 22:47:37 -0500

    Journal Page add note modal logic

---
+++ client/src/components/AddNoteModal.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+
+export const AddNoteModal = () => {
+    const API_URL = import.meta.env.VITE_API_URL;
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
--- client/src/pages/JournalPage.jsx
+++ client/src/pages/JournalPage.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { useState } from "react";
+import { AddNoteModal } from '../components/AddNoteModal'
 
 export const JournalPage = () => {
+  const [isAddNoteOpen, setIsAddNoteOpen] = useState(false);
+
   return (
     <div>
         <h1>JournalPage</h1>
         <p>View and add Joirnal here.</p>
+        <button
+          onClick={() => setIsAddNoteOpen(true)}
+        >
+          Add Tasting Note
+        </button>
+        {isAddNoteOpen && (
+          <AddNoteModal
+            onClose={() => setIsAddNoteOpen(false)}
+          />
+        )}
     </div>
   )
 }
