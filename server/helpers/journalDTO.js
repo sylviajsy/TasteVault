2be1a18c3af2c5bb420a35872765aff5f85676b1
Siyi Ji <sylviajsyy@gmail.com>
2026-05-14 16:03:19 -0500

    Fix Edge case 0 for DTO

--- server/helpers/journalDTO.js
+++ server/helpers/journalDTO.js
@@ -7,11 +7,11 @@ export const mapJournalInputDTO = (body, userId) => {
         wineId: body.wine_id,
         score: body.score,
         price: body.price,
-        acidity: body.user_acidity || 5,
-        fizziness: body.user_fizziness || 5,
-        intensity: body.user_intensity || 5,
-        sweetness: body.user_sweetness || 5,
-        tannin: body.user_tannin || 5,
+        acidity: body.user_acidity ?? 5,
+        fizziness: body.user_fizziness ?? 5,
+        intensity: body.user_intensity ?? 5,
+        sweetness: body.user_sweetness ?? 5,
+        tannin: body.user_tannin ?? 5,
         flavor: Array.isArray(body.user_flavor) ? body.user_flavor : [],
         comment: body.comment?.trim() || ""
     }
