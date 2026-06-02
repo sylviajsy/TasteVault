e91c65f1a928f6657786f7a3ea0a8e5697c5ed85
Siyi Ji <sylviajsyy@gmail.com>
2026-06-02 16:11:38 -0500

    Add VITE_AUTH0_AUDIENCE

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,7 +16,10 @@ createRoot(document.getElementById('root')).render(
       <Auth0Provider
         domain={domain}
         clientId={clientId}
-        authorizationParams={{ redirect_uri: window.location.origin }}
+        authorizationParams={{ 
+          redirect_uri: window.location.origin + '/',
+          audience: {audience}
+        }}
       >
         <App />
       </Auth0Provider>
