31f7210030503cd12c1c9861773ffb3919a58879
Siyi Ji <sylviajsyy@gmail.com>
2026-06-02 00:21:05 -0500

    Add Auth0 setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <App />
+      </Auth0Provider>
     </Provider>
   </StrictMode>,
 )
